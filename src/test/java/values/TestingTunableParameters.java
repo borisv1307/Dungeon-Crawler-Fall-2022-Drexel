9373ca16a845909a447c0a33c29d5dfee0198b33
Mark <mdoghram0@gmail.com>
2022-11-10 09:59:15 -0500

    Added line coverage for additions to Tunable Parameter.

--- src/test/java/values/TestingTunableParameters.java
+++ src/test/java/values/TestingTunableParameters.java
@@ -1,5 +1,15 @@
 package values;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static values.TunableParameters.PLAYER_SPEED;
+
 public class TestingTunableParameters {
-	public static final String FILE_LOCATION_PREFIX = "src/test/resources/levels/";
+    public static final String FILE_LOCATION_PREFIX = "src/test/resources/levels/";
+
+    @Test
+    public void player_speed_is_one() {
+        assertEquals(1, PLAYER_SPEED);
+    }
 }
