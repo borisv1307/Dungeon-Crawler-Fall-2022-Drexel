d024f08dfd3463c39f2ed7186879d659118d351e
Sarah Van Pelt <sbrown@meetedgar.com>
2022-11-10 18:20:48 -0600

    Added player movement to keyLeft(), keyRight(), keyUp(), keyDown() methods in GameEngine.java.

--- src/main/java/engine/GameEngine.java
+++ src/main/java/engine/GameEngine.java
@@ -1,7 +1,6 @@
 package engine;
 
-import java.awt.Component;
-import java.awt.Point;
+import java.awt.*;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,13 +10,13 @@ import ui.GameFrame;
 
 public class GameEngine {
 
-	private boolean exit;
 	private final LevelCreator levelCreator;
 	private final Map<Point, TileType> tiles = new HashMap<>();
+	private final int level;
+	private boolean exit;
 	private int levelHorizontalDimension;
 	private int levelVerticalDimension;
 	private Point player;
-	private final int level;
 
 	public GameEngine(LevelCreator levelCreator) {
 		exit = false;
@@ -41,20 +40,20 @@ public class GameEngine {
 		}
 	}
 
-	public void setLevelHorizontalDimension(int levelHorizontalDimension) {
-		this.levelHorizontalDimension = levelHorizontalDimension;
-	}
-
-	public void setLevelVerticalDimension(int levelVerticalDimension) {
-		this.levelVerticalDimension = levelVerticalDimension;
-	}
-
 	public int getLevelHorizontalDimension() {
 		return levelHorizontalDimension;
 	}
 
+	public void setLevelHorizontalDimension(int levelHorizontalDimension) {
+		this.levelHorizontalDimension = levelHorizontalDimension;
+	}
+
 	public int getLevelVerticalDimension() {
 		return levelVerticalDimension;
+	}
+
+	public void setLevelVerticalDimension(int levelVerticalDimension) {
+		this.levelVerticalDimension = levelVerticalDimension;
 	}
 
 	public TileType getTileFromCoordinates(int x, int y) {
@@ -74,26 +73,26 @@ public class GameEngine {
 	}
 
 	public void keyLeft() {
-		// TODO Implement movement logic here
+		setPlayer(getPlayerXCoordinate() - 1, getPlayerYCoordinate());
 	}
 
 	public void keyRight() {
-		// TODO Implement movement logic here
+		setPlayer(getPlayerXCoordinate() + 1, getPlayerYCoordinate());
 	}
 
 	public void keyUp() {
-		// TODO Implement movement logic here
+		setPlayer(getPlayerXCoordinate(), getPlayerYCoordinate() - 1);
 	}
 
 	public void keyDown() {
-		// TODO Implement movement logic here
-	}
-
-	public void setExit(boolean exit) {
-		this.exit = exit;
+		setPlayer(getPlayerXCoordinate(), getPlayerYCoordinate() + 1);
 	}
 
 	public boolean isExit() {
 		return exit;
+	}
+
+	public void setExit(boolean exit) {
+		this.exit = exit;
 	}
 }
--- src/test/java/engine/GameEngineTest.java
+++ src/test/java/engine/GameEngineTest.java
@@ -3,7 +3,7 @@ package engine;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
-import java.awt.Component;
+import java.awt.*;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -75,29 +75,5 @@ public class GameEngineTest {
 		gameEngine.setExit(exit);
 		boolean actual = gameEngine.isExit();
 		assertThat(actual, equalTo(exit));
-	}
-
-	@Test
-	public void key_left() {
-		// TODO Should I start with this test?
-		gameEngine.keyLeft();
-	}
-
-	@Test
-	public void key_right() {
-		// TODO Should I start with this test?
-		gameEngine.keyRight();
-	}
-
-	@Test
-	public void key_up() {
-		// TODO Should I start with this test?
-		gameEngine.keyUp();
-	}
-
-	@Test
-	public void key_down() {
-		// TODO Should I start with this test?
-		gameEngine.keyDown();
 	}
 }
