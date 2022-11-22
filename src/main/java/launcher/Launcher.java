e0d6e9641320cceea8a4154d6b746da3f2e7621a
Mark <mdoghram0@gmail.com>
2022-11-22 15:34:07 -0500

    Another Attempt to fix the headless issue.

--- src/main/java/launcher/Launcher.java
+++ src/main/java/launcher/Launcher.java
@@ -9,7 +9,13 @@ import wrappers.ThreadWrapper;
 
 public class Launcher {
 
+	static {
+		System.setProperty("java.awt.headless", "false");
+		System.out.println(java.awt.GraphicsEnvironment.isHeadless());
+	}
+
 	public static void main(String[] args) {
+
 		ThreadWrapper threadWrapper = ObjectFactory.getDefaultThreadWrapper();
 		GameEngine gameEngine = ObjectFactory.getDefaultGameEngine();
 		GameFrame gameFrame = ObjectFactory.getDefaultGameFrame();
