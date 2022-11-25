55c22f2c46c5c2e646acadd639e48ed803324f7f
Brad Donohue <bdonohue96@gmail.com>
2022-11-25 17:28:52 -0500

    Added color (RED) to projectile, tests passed

--- src/main/java/values/TileColorMap.java
+++ src/main/java/values/TileColorMap.java
@@ -1,20 +1,21 @@
 package values;
 
-import java.awt.Color;
+import java.awt.*;
 import java.util.EnumMap;
 
 import tiles.TileType;
 
 public final class TileColorMap {
-	private TileColorMap() {
-	}
-
 	private static final EnumMap<TileType, Color> tileColors = new EnumMap<>(TileType.class);
 
 	static {
 		tileColors.put(TileType.PASSABLE, Color.WHITE);
 		tileColors.put(TileType.NOT_PASSABLE, Color.BLACK);
 		tileColors.put(TileType.PLAYER, Color.GREEN);
+		tileColors.put(TileType.PROJECTILE, Color.RED);
+	}
+
+	private TileColorMap() {
 	}
 
 	public static Color get(TileType key) {
