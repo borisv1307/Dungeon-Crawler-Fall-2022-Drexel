a92a84abe47d3a412fb89eaa0e4315ea10802da9
mohamedahmed <mohamedsamir256@gmail.com>
2022-11-27 16:13:30 -0500

    Fix bug preventing Pipeline from building.

--- src/test/java/main/MovementStepDefs.java
+++ src/test/java/main/MovementStepDefs.java
@@ -5,12 +5,15 @@ import static org.hamcrest.Matchers.equalTo;
 
 import java.util.List;
 
+import org.mockito.Mockito;
+
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import engine.GameEngine;
 import parser.LevelCreationStepDefHelper;
 import parser.LevelCreator;
+import ui.ScorePanel;
 import values.TestingTunableParameters;
 import wrappers.ReaderWrapper;
 
@@ -21,39 +24,39 @@ public class MovementStepDefs extends LevelCreationStepDefHelper {
 	@Given("^the level design is:$")
 	public void level_is(List<String> levelStrings) throws Throwable {
 		writeLevelFile(levelStrings);
+		ScorePanel scorePanel = Mockito.mock(ScorePanel.class);
 		gameEngine = new GameEngine(
-				new LevelCreator(TestingTunableParameters.FILE_LOCATION_PREFIX, new ReaderWrapper()),
-				ObjectFactory.getDefaultScorePanel());
+				new LevelCreator(TestingTunableParameters.FILE_LOCATION_PREFIX, new ReaderWrapper()), scorePanel);
 	}
 
 	@When("^the player moves left$")
-	public void the_player_moves_left() throws Throwable {
+	public void the_player_moves_left() {
 		gameEngine.keyLeft();
 	}
 
 	@When("^the player moves right$")
-	public void the_player_moves_right() throws Throwable {
+	public void the_player_moves_right() {
 		gameEngine.keyRight();
 	}
 
 	@When("^the player moves up$")
-	public void the_player_moves_up() throws Throwable {
+	public void the_player_moves_up() {
 		gameEngine.keyUp();
 	}
 
 	@When("^the player moves down$")
-	public void the_player_moves_down() throws Throwable {
+	public void the_player_moves_down() {
 		gameEngine.keyDown();
 	}
 
 	@Then("^the player is located at \\((\\d+), (\\d+)\\)$")
-	public void the_player_is_located_at(int playerX, int playerY) throws Throwable {
+	public void the_player_is_located_at(int playerX, int playerY) {
 		assertThat(gameEngine.getPlayerXCoordinate(), equalTo(playerX - COORDINATE_OFFSET));
 		assertThat(gameEngine.getPlayerYCoordinate(), equalTo(playerY - COORDINATE_OFFSET));
 	}
 
 	@When("^the player moves up twice$")
-	public void the_player_moves_up_twice() throws Throwable {
+	public void the_player_moves_up_twice() {
 		gameEngine.keyUp();
 		gameEngine.keyUp();
 	}
