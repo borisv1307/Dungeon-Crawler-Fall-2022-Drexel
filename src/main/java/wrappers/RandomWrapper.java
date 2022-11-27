ce38427fd7297c83460d77187ada8c64acb2ddee
mohamedahmed <mohamedsamir256@gmail.com>
2022-11-26 23:34:14 -0500

    Use secure random number generator to address security vulnerability. Remove comment. Fix bug.

--- src/main/java/wrappers/RandomWrapper.java
+++ src/main/java/wrappers/RandomWrapper.java
@@ -1,10 +1,11 @@
 package wrappers;
 
-import java.util.Random;
+import java.security.SecureRandom;
 
 public class RandomWrapper {
+	private final SecureRandom random = new SecureRandom();
+
 	public int getRandomNumberInRange(int lower, int upper) {
-		Random random = new Random();
 		return random.nextInt(upper - lower + 1) + lower;
 	}
 }
