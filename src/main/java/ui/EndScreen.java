c6355c1bfe9190d79d138a56ea023f4f2ac8a5ce
mildredI <chinenye.idabor@aun.edu.ng>
2022-11-29 17:26:14 -0500

    duplicates fixed

--- src/main/java/ui/EndScreen.java
+++ src/main/java/ui/EndScreen.java
@@ -7,29 +7,29 @@ import java.awt.event.ActionListener;
 
 public class EndScreen extends JFrame implements ActionListener {
     private static final long serialVersionUID = 1L;
-    JRadioButton[] choose = new JRadioButton[1];
-    String[] chooseStrings = {"EXIT!"};
-    EndScreenPanel buttonPanel;
+    JRadioButton[] end = new JRadioButton[1];
+    String[] endStrings = {"EXIT!"};
+    EndScreenPanel endButtonPanel;
 
 
     public EndScreen() {
-        buttonPanel = new EndScreenPanel();
+        endButtonPanel = new EndScreenPanel();
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setBounds(320, 127, 600, 400);
         setResizable(false);
 
-        buttonPanel.setBackground(Color.WHITE);
+        endButtonPanel.setBackground(Color.WHITE);
 
-        for (int i = 0; i < choose.length; i++) {
-            choose[i] = new JRadioButton(chooseStrings[i]);
-            choose[i].addActionListener(this);
-            choose[i].setBackground(Color.WHITE);
-            choose[i].setBounds(230, 200 + i * 50, 200, 30);
-            buttonPanel.add(choose[i]);
+        for (int i = 0; i < end.length; i++) {
+            end[i] = new JRadioButton(endStrings[i]);
+            end[i].addActionListener(this);
+            end[i].setBackground(Color.WHITE);
+            end[i].setBounds(230, 200 + i * 50, 200, 30);
+            endButtonPanel.add(end[i]);
         }
 
-        buttonPanel.setLayout(null);
-        getContentPane().add(buttonPanel);
+        endButtonPanel.setLayout(null);
+        getContentPane().add(endButtonPanel);
         setLocationRelativeTo(null);
         setVisible(true);
     }
@@ -38,7 +38,7 @@ public class EndScreen extends JFrame implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         Object obj = e.getSource();
 
-        if (obj == choose[0]) {
+        if (obj == end[0]) {
             System.exit(0);
         }
 
