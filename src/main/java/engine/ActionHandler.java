f36ffb78e7b2480299d32f3fa699b88b95c73a31
Stephen Lin <yl3248@drexel.edu>
2022-11-28 22:14:21 -0500

    Create action handling for action logic along with action handler test.

---
+++ src/main/java/engine/ActionHandler.java
@@ -0,0 +1,64 @@
+package engine;
+
+import tiles.TileType;
+
+public class ActionHandler {
+
+    private GameEngine gameEngine;
+
+    public ActionHandler(GameEngine gameEngine) {
+        this.gameEngine = gameEngine;
+    }
+
+
+    public boolean tileIsPassable(int deltaX, int deltaY) {
+        TileType tileType = TileType.PASSABLE;
+        return checkTileIsEqual(tileType, convertPositionToTitleType(deltaX, deltaY));
+
+    }
+
+    public boolean tileIsNextLevel(int deltaX, int deltaY) {
+        TileType tileType = TileType.NEXT_LEVEL;
+        return checkTileIsEqual(tileType, convertPositionToTitleType(deltaX, deltaY));
+    }
+
+    private TileType convertPositionToTitleType(int deltaX, int deltaY) {
+        return gameEngine.getTileFromCoordinates(gameEngine.getPlayerXCoordinate() + deltaX, gameEngine.getPlayerYCoordinate() + deltaY);
+    }
+
+    private boolean checkTileIsEqual(TileType tileTypeOne, TileType tileTypeTwo) {
+        return tileTypeOne.equals(tileTypeTwo);
+    }
+
+    public void interactWithObject() {
+        if (playerIsNextToEnemy()) {
+            System.out.println("I am next to enemy");
+        }
+    }
+
+    private boolean playerIsNextToEnemy() {
+
+        int playerXCoordinate = gameEngine.getPlayerXCoordinate();
+        int playerYCoordinate = gameEngine.getPlayerYCoordinate();
+
+
+        return (playerIsBelowOrAboveTheEnemy(playerXCoordinate, playerYCoordinate) || playerIsLeftOrRightTheEnemy(playerXCoordinate, playerYCoordinate));
+
+    }
+
+    private boolean playerIsBelowOrAboveTheEnemy(int playerXCoordinate, int playerYCoordinate) {
+        boolean playerIsAboveEnemy = gameEngine.getTileFromCoordinates(playerXCoordinate, playerYCoordinate + 1).equals(TileType.ENEMY);
+        boolean playerIsBelowEnemy = gameEngine.getTileFromCoordinates(playerXCoordinate, playerYCoordinate - 1).equals(TileType.ENEMY);
+        return (playerIsAboveEnemy || playerIsBelowEnemy);
+
+    }
+
+    private boolean playerIsLeftOrRightTheEnemy(int playerXCoordinate, int playerYCoordinate) {
+        boolean playerIsLeftEnemy = gameEngine.getTileFromCoordinates(playerXCoordinate - 1, playerYCoordinate).equals(TileType.ENEMY);
+        boolean playerIsRightEnemy = gameEngine.getTileFromCoordinates(playerXCoordinate + 1, playerYCoordinate).equals(TileType.ENEMY);
+
+        return (playerIsLeftEnemy || playerIsRightEnemy);
+
+    }
+
+}
