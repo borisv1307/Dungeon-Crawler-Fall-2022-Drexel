c9b187900ba460f3da46e114345865a5f26b5272
Stephen Lin <81601143+Zero-stars@users.noreply.github.com>
2022-11-30 13:30:56 -0500

    Fixing the ActionHandlerTest to pass the Travis CI check

--- src/test/java/engine/ActionHandlerTest.java
+++ src/test/java/engine/ActionHandlerTest.java
@@ -1,22 +1,50 @@
 package engine;
 
-import main.ObjectFactory;
 import org.junit.Before;
 import org.junit.Test;
+import parser.LevelCreator;
 
-import static junit.framework.TestCase.assertSame;
+import wrappers.ReaderWrapper;
+
+import java.awt.*;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class ActionHandlerTest {
 
+    GameEngine gameEngine;
     ActionHandler actionHandler;
 
     @Before
-    public void setUp(){
-        actionHandler = new ActionHandler(ObjectFactory.getDefaultGameEngine());
+    public void setUp() {
+        LevelCreator levelCreator = new LevelCreator("src/main/resources/levels/", new ReaderWrapper());
+        gameEngine = new GameEngine(levelCreator);
+        actionHandler = new ActionHandler(gameEngine);
     }
 
     @Test
-    public void number_is_equal_one(){
-        int number =1;
-        assertSame(1,number);
+    public void tile_is_passable() {
+        boolean actual = actionHandler.tileIsPassable(1, 0);
+        assertEquals(true, actual);
+
     }
+
+    @Test
+    public void tile_is_not_passable() {
+        boolean actual = actionHandler.tileIsPassable(0, 1);
+        assertEquals(false, actual);
+    }
+
+    @Test
+    public void tile_is_next_level() {
+        boolean actual = actionHandler.tileIsNextLevel(-1, -8);
+        assertEquals(true, actual);
+    }
+
+    @Test
+    public void tile_is_not_next_level() {
+        boolean actual = actionHandler.tileIsNextLevel(-1, -1);
+        assertEquals(false, actual);
+    }
+
 }
