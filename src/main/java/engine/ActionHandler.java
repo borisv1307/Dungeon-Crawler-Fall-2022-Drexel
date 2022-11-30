043ad5d84d48efd5e2b9ba6f3ab10002f5bf7134
Stephen Lin <81601143+Zero-stars@users.noreply.github.com>
2022-11-29 22:39:36 -0500

    Re-add ActionHandler

--- src/main/java/engine/ActionHandler.java
+++ src/main/java/engine/ActionHandler.java
@@ -32,12 +32,33 @@ public class ActionHandler {
 
     public void interactWithObject() {
         if (playerIsNextToEnemy()) {
-            System.out.println("true");
+            //perform action like attack
         }
     }
 
-    public boolean playerIsNextToEnemy() {
-        return true;
+    private boolean playerIsNextToEnemy() {
+
+        int playerXCoordinate = gameEngine.getPlayerXCoordinate();
+        int playerYCoordinate = gameEngine.getPlayerYCoordinate();
+
+
+        return (playerIsBelowOrAboveTheEnemy(playerXCoordinate, playerYCoordinate) || playerIsLeftOrRightTheEnemy(playerXCoordinate, playerYCoordinate));
+
+    }
+
+    private boolean playerIsBelowOrAboveTheEnemy(int playerXCoordinate, int playerYCoordinate) {
+        boolean playerIsAboveEnemy = gameEngine.getTileFromCoordinates(playerXCoordinate, playerYCoordinate + 1).equals(TileType.ENEMY);
+        boolean playerIsBelowEnemy = gameEngine.getTileFromCoordinates(playerXCoordinate, playerYCoordinate - 1).equals(TileType.ENEMY);
+        return (playerIsAboveEnemy || playerIsBelowEnemy);
+
+    }
+
+    private boolean playerIsLeftOrRightTheEnemy(int playerXCoordinate, int playerYCoordinate) {
+        boolean playerIsLeftEnemy = gameEngine.getTileFromCoordinates(playerXCoordinate - 1, playerYCoordinate).equals(TileType.ENEMY);
+        boolean playerIsRightEnemy = gameEngine.getTileFromCoordinates(playerXCoordinate + 1, playerYCoordinate).equals(TileType.ENEMY);
+
+        return (playerIsLeftEnemy || playerIsRightEnemy);
+
     }
 
 }
