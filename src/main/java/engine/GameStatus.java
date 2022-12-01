bb3f520e6ad5cfc6ae22d1fe1903de5ded0cd7af
MF80 <MF80@users.noreply.github.com>
2022-11-30 22:00:04 -0500

    Add private constructor to GameStatus and change modifier order

--- src/main/java/engine/GameStatus.java
+++ src/main/java/engine/GameStatus.java
@@ -1,8 +1,12 @@
 package engine;
 
 public final class GameStatus {
-    public final static String PLAYER_RESPAWNED = "You have respawned.";
-    public final static String ENEMY_DEFEATED = "%s has been defeated.";
-    public final static String PLAYER_DEFEATED = "You have been defeated.";
-    public final static String DAMAGE_TO_ENEMY = "You attack the %s for %d.  Its armor deflects %d point of damage.";
+
+    public static final String PLAYER_RESPAWNED = "You have respawned.";
+    public static final String ENEMY_DEFEATED = "%s has been defeated.";
+    public static final String PLAYER_DEFEATED = "You have been defeated.";
+    public static final String DAMAGE_TO_ENEMY = "You attack the %s for %d.  Its armor deflects %d point of damage.";
+    private GameStatus() {
+        throw new IllegalStateException("Utility class");
+    }
 }
