9be45950dc9f89a91539a2154e85e933fd925b07
Dave Welsh <dw927@drexel.edu>
2022-12-02 14:48:39 -0500

    Added projectile graphics. Added projectile graphics test cases.

--- src/main/java/ui/TilePainter.java
+++ src/main/java/ui/TilePainter.java
@@ -1,33 +1,45 @@
 package ui;
 
-import java.awt.*;
-
 import engine.GameEngine;
+import projectile.Projectile;
 import tiles.TileType;
 import values.TileColorMap;
 
+import java.awt.*;
+
 public class TilePainter {
 
-	void paintTiles(Graphics graphics, GameEngine game, int tileWidth, int tileHeight) {
-		for (int x = 0; x < game.getLevelHorizontalDimension(); x++) {
-			for (int y = 0; y < game.getLevelVerticalDimension(); y++) {
-				TileType tileType = game.getTileFromCoordinates(x, y);
-				paintTile(graphics, tileWidth, tileHeight, x, y, tileType);
-			}
-		}
-	}
+    void paintTiles(Graphics graphics, GameEngine game, int tileWidth, int tileHeight) {
+        for (int x = 0; x < game.getLevelHorizontalDimension(); x++) {
+            for (int y = 0; y < game.getLevelVerticalDimension(); y++) {
+                TileType tileType = game.getTileFromCoordinates(x, y);
+                paintTile(graphics, tileWidth, tileHeight, x, y, tileType);
+            }
+        }
+    }
 
-	void paintPlayer(Graphics graphics, int x, int y, int tileWidth, int tileHeight, TileType tileType) {
-		paintTile(graphics, tileWidth, tileHeight, x, y, tileType);
-	}
+    void paintPlayer(Graphics graphics, int x, int y, int tileWidth, int tileHeight, TileType tileType) {
+        paintTile(graphics, tileWidth, tileHeight, x, y, tileType);
+    }
 
-	private void paintTile(Graphics graphics, int tileWidth, int tileHeight, int x, int y, TileType tileType) {
-		handleTile(graphics, tileType);
-		graphics.fillRect(x * tileWidth, y * tileHeight, tileWidth, tileHeight);
-	}
+    void paintProjectiles(Graphics graphics, int tileWidth, int tileHeight, GameEngine game) {
+        int x, y;
+        graphics.setColor(Color.BLUE);
+        for (Projectile i : game.getProjectiles()) {
+            x = i.getX();
+            y = i.getY();
+            graphics.fillOval(x * tileWidth, y * tileHeight, tileWidth, tileHeight);
+        }
+    }
 
-	private void handleTile(Graphics graphics, TileType tileType) {
-		graphics.setColor(TileColorMap.get(tileType));
-	}
+    private void paintTile(Graphics graphics, int tileWidth, int tileHeight, int x, int y, TileType tileType) {
+        handleTile(graphics, tileType);
+        graphics.fillRect(x * tileWidth, y * tileHeight, tileWidth, tileHeight);
+    }
+
+
+    private void handleTile(Graphics graphics, TileType tileType) {
+        graphics.setColor(TileColorMap.get(tileType));
+    }
 
 }
