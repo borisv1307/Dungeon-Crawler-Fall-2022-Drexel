e6323c0cc6fde4f564dba48fda62287ce38e8d44
Brad Donohue <bdonohue96@gmail.com>
2022-12-02 14:50:41 -0500

    Tested creating a projectile randomly on each border (left, right, top, bottom)

--- src/main/java/engine/ProjectileHandler.java
+++ src/main/java/engine/ProjectileHandler.java
@@ -19,4 +19,21 @@ public class ProjectileHandler {
 		gameEngine.addTile(0, yValue, TileType.PROJECTILE);
 	}
 
+	public void createRightEdgeProjectile() {
+		int maxY = gameEngine.getLevelVerticalDimension();
+		int yValue = randomWrapper.nextInt(maxY);
+		gameEngine.addTile(gameEngine.getLevelHorizontalDimension(), yValue, TileType.PROJECTILE);
+	}
+
+	public void createTopEdgeProjectile() {
+		int maxX = gameEngine.getLevelHorizontalDimension();
+		int xValue = randomWrapper.nextInt(maxX);
+		gameEngine.addTile(xValue, 0, TileType.PROJECTILE);
+	}
+
+	public void createBottomEdgeProjectile() {
+		int maxX = gameEngine.getLevelHorizontalDimension();
+		int xValue = randomWrapper.nextInt(maxX);
+		gameEngine.addTile(xValue, gameEngine.getLevelVerticalDimension(), TileType.PROJECTILE);
+	}
 }
--- src/test/java/engine/ProjectileHandlerTest.java
+++ src/test/java/engine/ProjectileHandlerTest.java
@@ -1,5 +1,6 @@
 package engine;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
@@ -7,15 +8,47 @@ import tiles.TileType;
 import wrappers.RandomWrapper;
 
 public class ProjectileHandlerTest {
-	@Test
-	public void generate_random_value() {
-		GameEngine gameEngine = Mockito.mock(GameEngine.class);
-		RandomWrapper randomWrapper = Mockito.mock(RandomWrapper.class);
-		ProjectileHandler projectileHandler = new ProjectileHandler(gameEngine, randomWrapper);
+
+	GameEngine gameEngine;
+	RandomWrapper randomWrapper;
+	ProjectileHandler projectileHandler;
+
+	@Before
+	public void setUp() {
+		gameEngine = Mockito.mock(GameEngine.class);
+		randomWrapper = Mockito.mock(RandomWrapper.class);
+		projectileHandler = new ProjectileHandler(gameEngine, randomWrapper);
 		gameEngine.setLevelVerticalDimension(10);
+		gameEngine.setLevelHorizontalDimension(20);
+	}
+
+	@Test
+	public void generate_projectile_left_side() {
 		Mockito.when(randomWrapper.nextInt(gameEngine.getLevelVerticalDimension())).thenReturn(6);
 		projectileHandler.createLeftEdgeProjectile();
 		Mockito.verify(gameEngine).addTile(0, 6, TileType.PROJECTILE);
+	}
 
+	@Test
+	public void generate_projectile_right_side() {
+		Mockito.when(randomWrapper.nextInt(gameEngine.getLevelVerticalDimension())).thenReturn(6);
+		Mockito.when(gameEngine.getLevelHorizontalDimension()).thenReturn(20);
+		projectileHandler.createRightEdgeProjectile();
+		Mockito.verify(gameEngine).addTile(20, 6, TileType.PROJECTILE);
+	}
+
+	@Test
+	public void generate_projectile_top_side() {
+		Mockito.when(randomWrapper.nextInt(gameEngine.getLevelHorizontalDimension())).thenReturn(10);
+		projectileHandler.createTopEdgeProjectile();
+		Mockito.verify(gameEngine).addTile(10, 0, TileType.PROJECTILE);
+	}
+
+	@Test
+	public void generate_projectile_bottom_side() {
+		Mockito.when(randomWrapper.nextInt(gameEngine.getLevelHorizontalDimension())).thenReturn(10);
+		Mockito.when(gameEngine.getLevelVerticalDimension()).thenReturn(10);
+		projectileHandler.createBottomEdgeProjectile();
+		Mockito.verify(gameEngine).addTile(10, 10, TileType.PROJECTILE);
 	}
 }
