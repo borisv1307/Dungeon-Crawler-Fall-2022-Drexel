7f0f0706c78255877cfdee3c490b46e832faf2c7
rsantangelo13 <rsantangelo13@gmail.com>
2022-12-02 02:54:53 -0500

    Fix security issue identified by Sonar

--- src/main/java/main/ObjectFactory.java
+++ src/main/java/main/ObjectFactory.java
@@ -13,11 +13,11 @@ import values.TunableParameters;
 import wrappers.SystemWrapper;
 import wrappers.ThreadWrapper;
 
-import java.util.Random;
+import java.security.SecureRandom;
 
 public abstract class ObjectFactory {
     private static ThreadWrapper defaultThreadWrapper = new ThreadWrapper();
-    private static LevelCreator defaultLevelCreator = new RandomLevelCreator(new BoardPieceFactory(), new Random(), 5, 20, 20);
+    private static LevelCreator defaultLevelCreator = new RandomLevelCreator(new BoardPieceFactory(), new SecureRandom(), 5, 20, 20);
     private static GameEngine defaultGameEngine = new GameEngine(defaultLevelCreator);
     private static GameFrame defaultGameFrame = new GameFrame(new GamePanel(defaultGameEngine, new TilePainter()),
             new WindowAdapterSystemExit(defaultGameEngine));
