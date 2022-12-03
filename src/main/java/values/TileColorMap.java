23ce9ad1684e20ae43a1177db32f967fe195fd74
Stephen Lin <81601143+Zero-stars@users.noreply.github.com>
2022-12-03 16:36:15 -0500

    Add feature where the player color changes based on the player current hp:
     Color green = more than 3 quarter Hp left
     Color Orange = more than half but less than 3 quarter left
      Color Blue = more than a quarter but less than half left
       Color Light gray = less than a quarter left.

--- src/main/java/values/TileColorMap.java
+++ src/main/java/values/TileColorMap.java
@@ -1,25 +1,49 @@
 package values;
 
+import entity.Player;
+import tiles.TileType;
+
 import java.awt.*;
 import java.util.EnumMap;
 
-import tiles.TileType;
-
 public final class TileColorMap {
-	private static final EnumMap<TileType, Color> tileColors = new EnumMap<>(TileType.class);
+    private static final EnumMap<TileType, Color> tileColors = new EnumMap<>(TileType.class);
 
-	static {
-		tileColors.put(TileType.PASSABLE, Color.WHITE);
-		tileColors.put(TileType.NOT_PASSABLE, Color.BLACK);
-		tileColors.put(TileType.PLAYER, Color.GREEN);
-		tileColors.put(TileType.ENEMY, Color.RED);
-		tileColors.put(TileType.NEXT_LEVEL, Color.pink);
-	}
+    static {
+        tileColors.put(TileType.PASSABLE, Color.WHITE);
+        tileColors.put(TileType.NOT_PASSABLE, Color.BLACK);
+        tileColors.put(TileType.PLAYER, Color.GREEN);
+        tileColors.put(TileType.ENEMY, Color.RED);
+        tileColors.put(TileType.NEXT_LEVEL, Color.pink);
+    }
 
-	private TileColorMap() {
-	}
+    private TileColorMap() {
+    }
 
-	public static Color get(TileType key) {
-		return tileColors.get(key);
-	}
+    public static Color get(TileType key) {
+        return tileColors.get(key);
+    }
+
+    public static void changePlayerHpBar(Player player) {
+        int playerCurrentHealth = player.getCurrentHealthPoint();
+        int maxHealthPoint = player.getMaxHealthPoint();
+        int quarter = maxHealthPoint / 4;
+        int half = maxHealthPoint / 2;
+        int threeQuarter = quarter * 3;
+
+        if (threeQuarter < playerCurrentHealth && playerCurrentHealth <= maxHealthPoint) {
+            tileColors.put(TileType.PLAYER, Color.GREEN);
+        }
+        if (half < playerCurrentHealth && playerCurrentHealth <= threeQuarter) {
+            tileColors.put(TileType.PLAYER, Color.ORANGE);
+        }
+        if (quarter < playerCurrentHealth && playerCurrentHealth <= half) {
+            tileColors.put(TileType.PLAYER, Color.BLUE);
+        }
+        if (playerCurrentHealth <= quarter) {
+            tileColors.put(TileType.PLAYER, Color.LIGHT_GRAY);
+        }
+
+    }
+
 }
--- src/test/java/values/TileColorMapTest.java
+++ src/test/java/values/TileColorMapTest.java
@@ -1,36 +1,87 @@
 package values;
 
-import static org.junit.Assert.assertSame;
+import entity.Player;
+import org.junit.Before;
+import org.junit.Test;
+import tiles.TileType;
 
 import java.awt.*;
 
-import org.junit.Test;
-
-import tiles.TileType;
+import static org.junit.Assert.assertSame;
 
 public class TileColorMapTest {
 
-	@Test
-	public void passable() {
-		assertSame(Color.WHITE, TileColorMap.get(TileType.PASSABLE));
-	}
+    Player player;
 
-	@Test
-	public void not_passable() {
-		assertSame(Color.BLACK, TileColorMap.get(TileType.NOT_PASSABLE));
-	}
+    @Before
+    public void setUp() {
+        player = new Player();
+    }
 
-	@Test
-	public void player() {
-		assertSame(Color.GREEN, TileColorMap.get(TileType.PLAYER));
-	}
-	@Test
-	public void enemy() {
-		assertSame(Color.RED, TileColorMap.get(TileType.ENEMY));
-	}
-	@Test
-	public void next_level(){
-		assertSame(Color.PINK, TileColorMap.get(TileType.NEXT_LEVEL));
-	}
+    @Test
+    public void passable() {
+        assertSame(Color.WHITE, TileColorMap.get(TileType.PASSABLE));
+    }
 
+    @Test
+    public void not_passable() {
+        assertSame(Color.BLACK, TileColorMap.get(TileType.NOT_PASSABLE));
+    }
+
+    @Test
+    public void player() {
+        assertSame(Color.GREEN, TileColorMap.get(TileType.PLAYER));
+    }
+
+    @Test
+    public void enemy() {
+        assertSame(Color.RED, TileColorMap.get(TileType.ENEMY));
+    }
+
+    @Test
+    public void next_level() {
+        assertSame(Color.PINK, TileColorMap.get(TileType.NEXT_LEVEL));
+    }
+
+    @Test
+    public void player_hp_color_no_change() {
+        player.takeDamage(0);
+        TileColorMap.changePlayerHpBar(player);
+        assertSame(Color.GREEN, TileColorMap.get(TileType.PLAYER));
+    }
+
+    @Test
+    public void player_hp_color_took_one_damage() {
+        player.takeDamage(1);
+        TileColorMap.changePlayerHpBar(player);
+        assertSame(Color.GREEN, TileColorMap.get(TileType.PLAYER));
+    }
+
+    @Test
+    public void player_hp_color_change_orange() {
+        player.takeDamage(4);
+        TileColorMap.changePlayerHpBar(player);
+        assertSame(Color.ORANGE, TileColorMap.get(TileType.PLAYER));
+    }
+
+    @Test
+    public void player_hp_color_change_blue() {
+        player.takeDamage(5);
+        TileColorMap.changePlayerHpBar(player);
+        assertSame(Color.BLUE, TileColorMap.get(TileType.PLAYER));
+    }
+
+    @Test
+    public void player_hp_color_change_light_gray() {
+        player.takeDamage(8);
+        TileColorMap.changePlayerHpBar(player);
+        assertSame(Color.LIGHT_GRAY, TileColorMap.get(TileType.PLAYER));
+    }
+
+    @Test
+    public void player_hp_color_in_negative() {
+        player.takeDamage(11);
+        TileColorMap.changePlayerHpBar(player);
+        assertSame(Color.LIGHT_GRAY, TileColorMap.get(TileType.PLAYER));
+    }
 }
