4090cf80a02c35b171f1ebbc760543350c9a1373
daniel_avery <davery@nwcath.org>
2022-12-03 11:23:38 -0500

    -Created PlayerPoint class extending Point which maintains hitpoints -Htipoints can be accessed through int gameEngine.getPlayerHitPoints() -Added new tile types PASSABLE_HARMFUL and PASSABLE_HELPFUL which affect hitpoints -Hitpoints are indicated by text in the upper left of the panel -Player tile was redesigned to visually show the player's health in green within a blue border

--- src/test/java/tiles/TileEffectsTest.java
+++ src/test/java/tiles/TileEffectsTest.java
@@ -1,14 +1,12 @@
 package tiles;
 
 import engine.GameEngine;
-import main.DungeonCrawler;
-import main.ObjectFactory;
 import org.junit.Before;
 import org.junit.Test;
-import timer.FramesPerSecondHandler;
-import ui.GameFrame;
+import parser.LevelCreator;
 import values.TileColorMap;
-import wrappers.ThreadWrapper;
+import values.TunableParameters;
+import wrappers.ReaderWrapper;
 
 import java.awt.*;
 
@@ -16,44 +14,35 @@ import static org.junit.Assert.assertEquals;
 
 public class TileEffectsTest {
 
-    ThreadWrapper threadWrapper;
     GameEngine gameEngine;
-    GameFrame gameFrame;
-    FramesPerSecondHandler framesPerSecondHandler;
 
     @Before
     public void setup() {
-        threadWrapper = ObjectFactory.getDefaultThreadWrapper();
-        gameEngine = ObjectFactory.getDefaultGameEngine();
-        gameFrame = ObjectFactory.getDefaultGameFrame();
-        framesPerSecondHandler = ObjectFactory.getDefaultFramesPerSecondHandler();
+        gameEngine = new GameEngine(new LevelCreator(TunableParameters.FILE_LOCATION_PREFIX,
+                new ReaderWrapper()));
     }
 
     @Test
     public void recognize_helpful_tile() {
 
-        new DungeonCrawler(threadWrapper, gameEngine, gameFrame, framesPerSecondHandler);
         assertEquals(TileType.PASSABLE_HELPFUL, gameEngine.getTileFromCoordinates(3, 2));
     }
 
     @Test
     public void recognize_helpful_tile_color() {
 
-        new DungeonCrawler(threadWrapper, gameEngine, gameFrame, framesPerSecondHandler);
         assertEquals(Color.PINK, TileColorMap.get(gameEngine.getTileFromCoordinates(3, 2)));
     }
 
     @Test
     public void recognize_harmful_tile() {
 
-        new DungeonCrawler(threadWrapper, gameEngine, gameFrame, framesPerSecondHandler);
         assertEquals(TileType.PASSABLE_HARMFUL, gameEngine.getTileFromCoordinates(10, 2));
     }
 
     @Test
     public void recognize_harmful_tile_color() {
 
-        new DungeonCrawler(threadWrapper, gameEngine, gameFrame, framesPerSecondHandler);
         assertEquals(Color.RED, TileColorMap.get(gameEngine.getTileFromCoordinates(10, 2)));
     }
 }
