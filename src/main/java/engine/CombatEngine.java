c3608af2deefb9569e3480871045a24774c05634
MF80 <MF80@users.noreply.github.com>
2022-12-02 19:04:28 -0500

    Clean-up combatEngine

--- src/main/java/engine/CombatEngine.java
+++ src/main/java/engine/CombatEngine.java
@@ -5,9 +5,7 @@ import entities.Player;
 import wrappers.RandomizerWrapper;
 
 public class CombatEngine {
-    RandomizerWrapper randomizerWrapper;
-    private Player player;
-    private Enemy enemy;
+    private RandomizerWrapper randomizerWrapper;
     private String gameStatusString;
 
     CombatEngine(RandomizerWrapper randomizerWrapper) {
@@ -15,38 +13,29 @@ public class CombatEngine {
     }
 
     public CombatObject doCombat(CombatObject combatObject) {
-        this.player = combatObject.player;
-        this.enemy = combatObject.enemy;
+        Player player = combatObject.player;
+        Enemy enemy = combatObject.enemy;
 
         int enemyHP = enemy.receiveDamage(player.getAttackValue());
         int playerHP = player.receiveDamage(enemy.getAttackValue());
-        gameStatusString = getCombatStatus(player, enemy);
+        setGameStatusString(getCombatStatus(player, enemy));
 
         if (enemyHP <= 0) {
-            this.enemy = killEnemy(enemy);
+            enemy = killEnemy(enemy);
         }
 
         if (playerHP <= 0) {
-            this.player = killPlayer(player);
+            player = killPlayer(player);
         }
 
-        return new CombatObject(this.player, this.enemy, gameStatusString);
-
+        return new CombatObject(player, enemy, gameStatusString);
     }
 
     Enemy killEnemy(Enemy enemy) {
-        gameStatusString = String.format(GameStatus.ENEMY_DEFEATED, enemy.getName());
+        setGameStatusString(String.format(GameStatus.ENEMY_DEFEATED, enemy.getName()));
         return createNewEnemy(0, 0);
     }
-
-    private Enemy createNewEnemy(int x, int y) {
-        return randomizerWrapper.getRandomEnemy(x, y);
-    }
-
-    private String getCombatStatus(Player player, Enemy enemy) {
-        return String.format(GameStatus.DAMAGE_TO_ENEMY, enemy.getName(), player.getAttackValue(), enemy.getArmorClass());
-    }
-
+    
     Player killPlayer(Player player) {
         setGameStatusString(GameStatus.PLAYER_DEFEATED);
         return new Player(player.getOriginX(), player.getOriginY());
@@ -56,7 +45,15 @@ public class CombatEngine {
         return gameStatusString;
     }
 
-    void setGameStatusString(String string) {
+    private void setGameStatusString(String string) {
         this.gameStatusString = string;
+    }
+
+    private Enemy createNewEnemy(int x, int y) {
+        return randomizerWrapper.getRandomEnemy(x, y);
+    }
+
+    private String getCombatStatus(Player player, Enemy enemy) {
+        return String.format(GameStatus.DAMAGE_TO_ENEMY, enemy.getName(), player.getAttackValue(), enemy.getArmorClass());
     }
 }
