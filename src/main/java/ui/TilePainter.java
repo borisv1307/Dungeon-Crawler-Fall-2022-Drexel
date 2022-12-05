9dd032ade3ad2d8cb9ebdcb3658534f860aa2d83
KahnTraversi <mlk367@drexel.edu>
2022-12-05 12:35:18 -0600

    The UI will now paint a visual and text update on player health, as well as a notification on the player character when regen and/or drain is on.

--- src/main/java/ui/TilePainter.java
+++ src/main/java/ui/TilePainter.java
@@ -1,33 +1,53 @@
 package ui;
 
-import java.awt.*;
-
 import engine.GameEngine;
 import tiles.TileType;
 import values.TileColorMap;
 
+import java.awt.*;
+
 public class TilePainter {
 
-	void paintTiles(Graphics graphics, GameEngine game, int tileWidth, int tileHeight) {
-		for (int x = 0; x < game.getLevelHorizontalDimension(); x++) {
-			for (int y = 0; y < game.getLevelVerticalDimension(); y++) {
-				TileType tileType = game.getTileFromCoordinates(x, y);
-				paintTile(graphics, tileWidth, tileHeight, x, y, tileType);
-			}
-		}
-	}
+    void paintTiles(Graphics graphics, GameEngine game, int tileWidth, int tileHeight) {
+        for (int x = 0; x < game.getLevelHorizontalDimension(); x++) {
+            for (int y = 0; y < game.getLevelVerticalDimension(); y++) {
+                TileType tileType = game.getTileFromCoordinates(x, y);
+                paintTile(graphics, tileWidth, tileHeight, x, y, tileType);
+            }
+        }
+    }
 
-	void paintPlayer(Graphics graphics, int x, int y, int tileWidth, int tileHeight, TileType tileType) {
-		paintTile(graphics, tileWidth, tileHeight, x, y, tileType);
-	}
+    void paintPlayer(Graphics graphics, int x, int y, int tileWidth, int tileHeight, TileType tileType, int currentHitPoints, int maxHitPoints, boolean regenStatus, boolean drainStatus) {
+        paintTile(graphics, tileWidth, tileHeight, x, y, tileType);
+        paintLostPlayerHitPoints(graphics, tileWidth, tileHeight, x, y, currentHitPoints, maxHitPoints);
+        paintPlayerStatus(graphics, tileWidth, tileHeight, x, y, currentHitPoints, regenStatus, drainStatus);
+    }
 
-	private void paintTile(Graphics graphics, int tileWidth, int tileHeight, int x, int y, TileType tileType) {
-		handleTile(graphics, tileType);
-		graphics.fillRect(x * tileWidth, y * tileHeight, tileWidth, tileHeight);
-	}
+    private void paintPlayerStatus(Graphics graphics, int tileWidth, int tileHeight, int x, int y, int currentHitPoints, boolean regenStatus, boolean drainStatus) {
+        graphics.setColor(Color.BLACK);
+        graphics.drawString("HP:" + currentHitPoints, x * tileWidth, y * tileHeight + 10);
+        if (regenStatus) {
+            graphics.drawString("Regen", x * tileWidth, y * tileHeight + 20);
+        }
+        if (drainStatus) {
+            graphics.drawString("Drain", x * tileWidth, y * tileHeight + 30);
+        }
+    }
 
-	private void handleTile(Graphics graphics, TileType tileType) {
-		graphics.setColor(TileColorMap.get(tileType));
-	}
+    private void paintLostPlayerHitPoints(Graphics graphics, int tileWidth, int tileHeight, int x, int y, int currentHitPoints, int maxHitPoints) {
+        double lostHitPointsPercentage = (maxHitPoints - currentHitPoints) / ((double) maxHitPoints);
+        graphics.setColor(Color.RED);
+        graphics.fillRect(x * tileWidth, y * tileHeight, tileWidth, (int) (tileHeight * lostHitPointsPercentage));
+
+    }
+
+    private void paintTile(Graphics graphics, int tileWidth, int tileHeight, int x, int y, TileType tileType) {
+        handleTile(graphics, tileType);
+        graphics.fillRect(x * tileWidth, y * tileHeight, tileWidth, tileHeight);
+    }
+
+    private void handleTile(Graphics graphics, TileType tileType) {
+        graphics.setColor(TileColorMap.get(tileType));
+    }
 
 }
--- src/test/java/ui/TilePainterTest.java
+++ src/test/java/ui/TilePainterTest.java
@@ -1,63 +1,141 @@
 package ui;
 
-import java.awt.*;
-
+import engine.GameEngine;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.AdditionalMatchers;
 import org.mockito.InOrder;
 import org.mockito.Matchers;
 import org.mockito.Mockito;
-
-import engine.GameEngine;
 import tiles.TileType;
 import values.TileColorMap;
 
+import java.awt.*;
+
 public class TilePainterTest {
 
-	private final int TILE_WIDTH = 10;
-	private final int TILE_HEIGHT = 20;
-	private final int X = 2;
-	private final int Y = 3;
+    private final int TILE_WIDTH = 10;
+    private final int TILE_HEIGHT = 20;
+    private final int X = 2;
+    private final int Y = 3;
 
-	Graphics graphics;
-	TilePainter tilePainter;
+    Graphics graphics;
+    TilePainter tilePainter;
 
-	@Before
-	public void setUp() {
-		tilePainter = new TilePainter();
-		graphics = Mockito.mock(Graphics.class);
-	}
+    @Before
+    public void setUp() {
+        tilePainter = new TilePainter();
+        graphics = Mockito.mock(Graphics.class);
+    }
 
-	@Test
-	public void paint_tiles() {
-		GameEngine game = Mockito.mock(GameEngine.class);
-		Mockito.when(game.getLevelHorizontalDimension()).thenReturn(X);
-		Mockito.when(game.getLevelVerticalDimension()).thenReturn(Y);
-		Mockito.when(game.getTileFromCoordinates(1, 1)).thenReturn(TileType.NOT_PASSABLE);
-		Mockito.when(game.getTileFromCoordinates(AdditionalMatchers.not(Matchers.eq(1)),
-				AdditionalMatchers.not(Matchers.eq(1)))).thenReturn(TileType.PASSABLE);
+    @Test
+    public void paint_tiles() {
+        GameEngine game = Mockito.mock(GameEngine.class);
+        Mockito.when(game.getLevelHorizontalDimension()).thenReturn(X);
+        Mockito.when(game.getLevelVerticalDimension()).thenReturn(Y);
+        Mockito.when(game.getTileFromCoordinates(1, 1)).thenReturn(TileType.NOT_PASSABLE);
+        Mockito.when(game.getTileFromCoordinates(AdditionalMatchers.not(Matchers.eq(1)),
+                AdditionalMatchers.not(Matchers.eq(1)))).thenReturn(TileType.PASSABLE);
 
-		tilePainter.paintTiles(graphics, game, TILE_WIDTH, TILE_HEIGHT);
+        tilePainter.paintTiles(graphics, game, TILE_WIDTH, TILE_HEIGHT);
 
-		InOrder inOrder = Mockito.inOrder(graphics);
-		inOrder.verify(graphics).setColor(TileColorMap.get(TileType.PASSABLE));
-		inOrder.verify(graphics).fillRect(0, 0, 10, 20);
-		inOrder.verify(graphics).fillRect(0, 20, 10, 20);
-		inOrder.verify(graphics).fillRect(0, 40, 10, 20);
-		inOrder.verify(graphics).fillRect(10, 0, 10, 20);
-		inOrder.verify(graphics).setColor(TileColorMap.get(TileType.NOT_PASSABLE));
-		inOrder.verify(graphics).fillRect(10, 20, 10, 20);
-		inOrder.verify(graphics).fillRect(10, 40, 10, 20);
+        InOrder inOrder = Mockito.inOrder(graphics);
+        inOrder.verify(graphics).setColor(TileColorMap.get(TileType.PASSABLE));
+        inOrder.verify(graphics).fillRect(0, 0, 10, 20);
+        inOrder.verify(graphics).fillRect(0, 20, 10, 20);
+        inOrder.verify(graphics).fillRect(0, 40, 10, 20);
+        inOrder.verify(graphics).fillRect(10, 0, 10, 20);
+        inOrder.verify(graphics).setColor(TileColorMap.get(TileType.NOT_PASSABLE));
+        inOrder.verify(graphics).fillRect(10, 20, 10, 20);
+        inOrder.verify(graphics).fillRect(10, 40, 10, 20);
 
-	}
+    }
 
-	@Test
-	public void paint_player() {
+    @Test
+    public void paint_player() {
 
-		tilePainter.paintPlayer(graphics, X, Y, TILE_WIDTH, TILE_HEIGHT, TileType.PLAYER);
+        tilePainter.paintPlayer(graphics, X, Y, TILE_WIDTH, TILE_HEIGHT, TileType.PLAYER, 10, 50, false, false);
 
-		Mockito.verify(graphics).fillRect(20, 60, 10, 20);
-	}
+        InOrder inOrder = Mockito.inOrder(graphics);
+        inOrder.verify(graphics).setColor(TileColorMap.get(TileType.PLAYER));
+        inOrder.verify(graphics).fillRect(20, 60, 10, 20);
+        inOrder.verify(graphics).setColor(Color.RED);
+        inOrder.verify(graphics).fillRect(20, 60, 10, 16);
+        inOrder.verify(graphics).setColor(Color.BLACK);
+        inOrder.verify(graphics).drawString("HP:10", 20, 70);
+    }
 
+    @Test
+    public void paint_player_max_health() {
+
+        tilePainter.paintPlayer(graphics, X, Y, TILE_WIDTH, TILE_HEIGHT, TileType.PLAYER, 50, 50, false, false);
+
+        InOrder inOrder = Mockito.inOrder(graphics);
+        inOrder.verify(graphics).setColor(TileColorMap.get(TileType.PLAYER));
+        inOrder.verify(graphics).fillRect(20, 60, 10, 20);
+        inOrder.verify(graphics).setColor(Color.RED);
+        inOrder.verify(graphics).fillRect(20, 60, 10, 0);
+        inOrder.verify(graphics).setColor(Color.BLACK);
+        inOrder.verify(graphics).drawString("HP:50", 20, 70);
+    }
+
+    @Test
+    public void paint_player_zero_health() {
+
+        tilePainter.paintPlayer(graphics, X, Y, TILE_WIDTH, TILE_HEIGHT, TileType.PLAYER, 0, 50, false, false);
+
+        InOrder inOrder = Mockito.inOrder(graphics);
+        inOrder.verify(graphics).setColor(TileColorMap.get(TileType.PLAYER));
+        inOrder.verify(graphics).fillRect(20, 60, 10, 20);
+        inOrder.verify(graphics).setColor(Color.RED);
+        inOrder.verify(graphics).fillRect(20, 60, 10, 20);
+        inOrder.verify(graphics).setColor(Color.BLACK);
+        inOrder.verify(graphics).drawString("HP:0", 20, 70);
+    }
+
+    @Test
+    public void paint_player_regen_on() {
+
+        tilePainter.paintPlayer(graphics, X, Y, TILE_WIDTH, TILE_HEIGHT, TileType.PLAYER, 10, 50, true, false);
+
+        InOrder inOrder = Mockito.inOrder(graphics);
+        inOrder.verify(graphics).setColor(TileColorMap.get(TileType.PLAYER));
+        inOrder.verify(graphics).fillRect(20, 60, 10, 20);
+        inOrder.verify(graphics).setColor(Color.RED);
+        inOrder.verify(graphics).fillRect(20, 60, 10, 16);
+        inOrder.verify(graphics).setColor(Color.BLACK);
+        inOrder.verify(graphics).drawString("HP:10", 20, 70);
+        inOrder.verify(graphics).drawString("Regen", 20, 80);
+    }
+
+    @Test
+    public void paint_player_drain_on() {
+
+        tilePainter.paintPlayer(graphics, X, Y, TILE_WIDTH, TILE_HEIGHT, TileType.PLAYER, 10, 50, false, true);
+
+        InOrder inOrder = Mockito.inOrder(graphics);
+        inOrder.verify(graphics).setColor(TileColorMap.get(TileType.PLAYER));
+        inOrder.verify(graphics).fillRect(20, 60, 10, 20);
+        inOrder.verify(graphics).setColor(Color.RED);
+        inOrder.verify(graphics).fillRect(20, 60, 10, 16);
+        inOrder.verify(graphics).setColor(Color.BLACK);
+        inOrder.verify(graphics).drawString("HP:10", 20, 70);
+        inOrder.verify(graphics).drawString("Drain", 20, 90);
+    }
+
+    @Test
+    public void paint_player_regen_and_drain_on() {
+
+        tilePainter.paintPlayer(graphics, X, Y, TILE_WIDTH, TILE_HEIGHT, TileType.PLAYER, 10, 50, true, true);
+
+        InOrder inOrder = Mockito.inOrder(graphics);
+        inOrder.verify(graphics).setColor(TileColorMap.get(TileType.PLAYER));
+        inOrder.verify(graphics).fillRect(20, 60, 10, 20);
+        inOrder.verify(graphics).setColor(Color.RED);
+        inOrder.verify(graphics).fillRect(20, 60, 10, 16);
+        inOrder.verify(graphics).setColor(Color.BLACK);
+        inOrder.verify(graphics).drawString("HP:10", 20, 70);
+        inOrder.verify(graphics).drawString("Regen", 20, 80);
+        inOrder.verify(graphics).drawString("Drain", 20, 90);
+    }
 }
